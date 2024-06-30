b6d52c2e36101fd13c97aa3fabd7a969ad20c5da
prerana1821 <prerananawar1@gmail.com>
2024-06-30 06:24:05 +0530

    fix(server) - cors issue

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const socket = require("socket.io");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.use(express.static("./client"));
 
